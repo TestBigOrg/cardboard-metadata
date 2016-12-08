13cd5dfcb5007e41af44b21e312ea281db492f10
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-12-08 17:28:44 -0500

    first working draft

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Dyno = require('dyno');
+var makeKey = require('./lib/make-key');
+var zoomRange = require('./lib/zoom-range');
 
 module.exports = function(config) {
 
@@ -12,13 +14,34 @@ module.exports = function(config) {
      * Create DynamoDB tables with Cardboard's schema
      * @param {function} callback - the callback function to handle the response
      */
-    cardboard.createTable = function(callback) {
+    cardboardMetadata.createTable = function(callback) {
         var tableSchema = require('./lib/metadata-table.json');
         tableSchema.TableName = config.metadataTable;
         config.dyno.createTable(tableSchema, callback);
     };        
 
-    cardboardMetadata.streamHandler = require('./lib/stream-handler')(config);
+    cardboardMetadata.get = function(dataset, callback) {
+        config.dyno.getItem({Key: makeKey(dataset) }, function(err, data) {
+            if (err) return callback(err);
+            if (data.Item === undefined) return callback(null, {});
+            var info = {
+                count: data.Item.count,     
+                size: data.Item.size,     
+                edits: data.Item.edits,
+                west: data.Item.west,
+                south: data.Item.south,
+                east: data.Item.east,
+                north: data.Item.north,
+                updated: data.Item.updated
+            };
+            var range = zoomRange(info.size, [info.west, info.south, info.east, info.north]);
+            info.minzoom = range.min;
+            info.maxzoom = range.max;
+            callback(null, info);
+        });
+    }
+
+    cardboardMetadata.streamHandler = require('./lib/stream-handler')(config, cardboardMetadata);
 
     return cardboardMetadata;
 }
---
+++ test/test.test.js
@@ -0,0 +1,108 @@
+var extent = require('@turf/bbox');
+var Dyno = require('dyno');
+var dynamodbTest = require('dynamodb-test');
+var tape = require('tape');
+var utils = require('cardboard/lib/utils')();
+
+var metadataTableSpec = require('../lib/metadata-table.json');
+var metadataTable = dynamodbTest(tape, 'cardboard-metadata', metadataTableSpec);
+
+var CardboardMetadata = require('..');
+
+var metadataConfig = {
+    metadataTable: metadataTable.tableName,
+    endpoint: 'http://localhost:4567',
+    region: 'test'
+};
+
+metadataTable.start();
+
+var states = require('./data/states.json').features.map(function(state) {
+    state.id = state.properties.name.toLowerCase().replace(/ /g, '-') + '-state';
+    return utils.toDatabaseRecord(state, 'default');
+});
+
+var countries = require('./data/countries.json').features.map(function(feature) {
+    feature.id = feature.properties.name.toLowerCase().replace(/ /g, '-') + '-country';
+    return utils.toDatabaseRecord(feature, 'default');
+});
+
+metadataTable.test('[stream handler] check adding on an empty db works', function(assert) {
+    var bounds = extent(require('./data/states.json')).map(function(v) {
+        return parseFloat(v.toFixed(6));
+    });
+    var cardboardMetadata = CardboardMetadata(metadataConfig);
+    var events = toEvent('INSERT', states);
+    var start = +new Date();
+    cardboardMetadata.streamHandler(events, function(err) {
+        if (err) return assert.end(err, 'stream worked');
+        cardboardMetadata.get('default', function(err, metadata) {
+            if (err) return assert.end(err);
+            assert.equal(metadata.count, states.length, 'has right count');
+            assert.equal(metadata.edits, states.length, 'has right num edits');
+            assert.equal(metadata.size, states.reduce(function(m, s) { return m + s.size; }, 0), 'has right size');
+            assert.equal(metadata.west, bounds[0], 'has right west');
+            assert.equal(metadata.south, bounds[1], 'has right south');
+            assert.equal(metadata.east, bounds[2], 'has right east');
+            assert.equal(metadata.north, bounds[3], 'has right north');
+            assert.equal(new Date(metadata.updated) > start, true, 'is marked as updated');
+            assert.end();
+        });
+    });
+});
+
+var seed = {dataset: 'default', key: 'metadata', count: countries.length+10, size: 10000000, west:0, east:0, south:0, north:0, edits:0 };
+metadataTable.test('[stream handler] check deleteing works', [seed], function(assert) {
+    var cardboardMetadata = CardboardMetadata(metadataConfig);
+    var events = toEvent('REMOVE', countries);
+    var start = +new Date();
+    cardboardMetadata.streamHandler(events, function(err) {
+        if (err) return assert.end(err, 'stream worked');
+        cardboardMetadata.get('default', function(err, metadata) {
+            if (err) return assert.end(err);
+            assert.equal(metadata.count, 10, 'has right count');
+            assert.equal(metadata.edits, countries.length, 'has right num edits');
+            assert.equal(metadata.size, seed.size - countries.reduce(function(m, s) { return m + s.size; }, 0), 'has right size');
+            assert.equal(metadata.west, 0, 'no change on west');
+            assert.equal(metadata.south, 0, 'no change on south');
+            assert.equal(metadata.east, 0, 'no change on east');
+            assert.equal(metadata.north, 0, 'no change on north');
+            assert.equal(new Date(metadata.updated) > start, true, 'is marked as updated');
+            assert.end();
+        });
+    });
+});
+
+metadataTable.test('[get] get an non-existing metadata doc', [seed], function(assert) {
+    var cardboardMetadata = CardboardMetadata(metadataConfig);
+    cardboardMetadata.get('nope-default', function(err, metadata) {
+        if (err) return assert.end(err);
+        assert.equal(Object.keys(metadata).length, 0, 'empty object');
+        assert.end();
+    });
+});
+
+metadataTable.test('[get] get an existing metadata doc', [seed], function(assert) {
+    var cardboardMetadata = CardboardMetadata(metadataConfig);
+    cardboardMetadata.get('default', function(err, metadata) {
+        if (err) return assert.end(err);
+        Object.keys(seed).forEach(function(key) {
+            if (key === 'dataset' || key === 'key') return;
+            assert.equal(metadata[key], seed[key], 'has '+key);     
+        });
+        assert.end();
+    });
+});
+
+metadataTable.close();
+
+function toEvent(action, records) {
+    return records.map(function(mainRecord) {
+        var serialized = JSON.parse(Dyno.serialize(mainRecord));
+        var record = { eventName: action };
+        record.dynamodb = {};
+        record.dynamodb.OldImage = action !== 'INSERT' ? serialized : undefined;
+        record.dynamodb.NewImage = action !== 'REMOVE' ? serialized : undefined;
+        return record;
+    });
+}
